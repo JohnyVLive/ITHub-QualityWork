4e1ade4d8d4839d4ed762f0da89c718eb5bfb36e
Evgeniy V. Radchenko <johny.live@koshmart.ru>
2026-01-18 21:46:52 +0300

    feat: add AdminPanel component and routing for admin functionalities

---
+++ corporate-directory/frontend/src/components/AdminPanel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="admin-container">
+        <div class="admin-header">
+            <h1>Панель администратора</h1>
+        </div>
+
+        <div class="admin-content">
+            <p>Здесь будут административные функции.</p> 
+        </div> 
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'AdminPanel',
+};
+</script>
--- corporate-directory/frontend/src/router/index.js
+++ corporate-directory/frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DirectoryView from '../components/DirectoryView.vue'
+import AdminPanel from '../components/AdminPanel.vue'
 
 const routes = [
     {
         path: '/',
         name: 'Directory',
         component: DirectoryView
+    },
+    {
+        path: '/admin',
+        name: 'AdminPanel',
+        component: AdminPanel
     }
 ]
 
