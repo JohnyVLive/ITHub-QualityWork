d5aa8b9737242a74dcd00f2a7a23484d4555456a
Evgeniy V. Radchenko <johny.live@koshmart.ru>
2026-01-24 16:00:22 +0300

    feat: add authentication requirement to AdminPanel route

--- corporate-directory/frontend/src/router/index.js
+++ corporate-directory/frontend/src/router/index.js
@@ -12,7 +12,8 @@ const routes = [
     {
         path: '/admin',
         name: 'AdminPanel',
-        component: AdminPanel
+        component: AdminPanel,
+        meta: { requiresAuth: true }
     },
     {
         path: '/login',
